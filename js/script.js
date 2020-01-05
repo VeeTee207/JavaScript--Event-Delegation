80851722f1ff67f207cfd793dbb5e61d1ebaf2aa
vterehov207 <v.terehov207@gmail.com>
2020-01-05 02:02:33 -0500

    Step#2 -- reversed timer added

--- js/script.js
+++ js/script.js
@@ -34,4 +34,51 @@ window.addEventListener('DOMContentLoaded', function() {
         }
 
     });
+
+    // Timer  . The setting poit of deadline deadLine variable
+    // It displays how many hours , minutes, seconds left until the dead line
+
+    let deadLine = '2020-01-25 01:00';   
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000)%60) ,
+        minutes = Math.floor(((t/1000)/60)%60) ,
+        hours = Math.floor(((t/1000)/60)/60) ;
+        // temp = Math.floor((10000/1000)%60) ;
+        // let time = new Date().getTime(); 
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+         } ;
+    } 
+
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id), 
+        hours = document.querySelector(".hours"),
+        minutes = document.querySelector(".minutes") ,
+        seconds = document.querySelector('.seconds') ,
+        timeInterval = setInterval(updateClock, 1000) ;
+    
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+                hours.textContent = t.hours;
+                minutes.textContent = t.minutes;
+                seconds.textContent = t.seconds;
+            if (t.total<=0) {
+                clearInterval(timeInterval) ;
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+
+            }
+        }   
+    }
+    setClock('time', deadLine) ;
+
+
 });
